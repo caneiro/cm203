a831bea3fa9c53d0c0adef9f413881f80628169f
caneiro <rafael.caneiro@gmail.com>
2022-12-12 19:29:58 -0300

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Trabalho Final Disciplina CM-203 - ITA',
+    author='Rafael Caneiro',
+    license='MIT',
+)
